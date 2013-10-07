a01a9dbadfc242bf303d88d765c826e64be2ddde
Mark Ludwig <mk.ludwig1@gmail.com>
2013-10-06 21:56:35 -0700

    Second initial commit

--- QtGUI/StartScreen.cpp
+++
@@ -1,9 +0,0 @@
-#include "StartScreen.hpp"
-
-StartScreen::StartScreen(QWidget* parent) : QWidget(parent)
-{
-    QPalette pal = palette();
-    pal.setBrush(QPalette::Background, QColor(255,255,255));
-    setAutoFillBackground(true);
-    setPalette(pal);
-};
