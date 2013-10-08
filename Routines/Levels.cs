71418d2c0d6b4f6c5ddc0acd914deef121951d71
Mark Ludwig <mk.ludwig1@gmail.com>
2013-10-07 18:23:23 -0700

    Change consts to enumerations

--- Routines/Levels.cs
+++ Routines/Levels.cs
@@ -85,47 +85,53 @@ namespace DriverLevelEditor.Driver
             OnLevelDestroyed(new DriverEventArgs("SomeLevel.lev"));
         }
 
-        public const uint LEV_TEXTURES                = 0x1;
-        public const uint LEV_MODELS                  = 0x2;
-        public const uint LEV_WORLD                   = 0x4;
-        public const uint LEV_RANDOM_MODEL_PLACEMENT  = 0x10;
-        public const uint LEV_TEXTURE_DEFINITIONS     = 0x20;
-        public const uint LEV_ROAD_TABLE              = 0x80;
-        public const uint LEV_ROAD_CONNECTIONS        = 0x100;
-        public const uint LEV_INTERSECTIONS           = 0x200;
-        public const uint LEV_HEIGHTMAP_TILES         = 0x400;
-        public const uint LEV_HEIGHTMAP               = 0x800;
-        public const uint LEV_MODEL_NAMES             = 0x1000;
-        public const uint LEV_EVENT_MODELS            = 0x2000;
-        public const uint LEV_VISIBILITY              = 0x4000;
-        public const uint LEV_SECTOR_TEXTURE_USAGE    = 0x8000;
-        public const uint LEV_ROAD_SECTIONS           = 0x10000;
-        public const uint LEV_INTERSECTION_POSITIONS  = 0x20000;
-        public const uint LEV_LAMPS                   = 0x80000;
-        public const uint LEV_CHAIR_PLACEMENT         = 0x100000;
-        public const uint LEV_ALL                     = 0xFFFFFFFF;
+        public enum BlockLoadFlags
+        {
+            LEV_TEXTURES                = 0x1,
+            LEV_MODELS                  = 0x2,
+            LEV_WORLD                   = 0x4,
+            LEV_RANDOM_MODEL_PLACEMENT  = 0x10,
+            LEV_TEXTURE_DEFINITIONS     = 0x20,
+            LEV_ROAD_TABLE              = 0x80,
+            LEV_ROAD_CONNECTIONS        = 0x100,
+            LEV_INTERSECTIONS           = 0x200,
+            LEV_HEIGHTMAP_TILES         = 0x400,
+            LEV_HEIGHTMAP               = 0x800,
+            LEV_MODEL_NAMES             = 0x1000,
+            LEV_EVENT_MODELS            = 0x2000,
+            LEV_VISIBILITY              = 0x4000,
+            LEV_SECTOR_TEXTURE_USAGE    = 0x8000,
+            LEV_ROAD_SECTIONS           = 0x10000,
+            LEV_INTERSECTION_POSITIONS  = 0x20000,
+            LEV_LAMPS                   = 0x80000,
+            LEV_CHAIR_PLACEMENT         = 0x100000,
+            LEV_ALL                     = 0xFFFFFFFF,
 
-        public const uint LEV_TRAFFIC = LEV_ROAD_CONNECTIONS | LEV_ROAD_SECTIONS | LEV_INTERSECTIONS | LEV_INTERSECTION_POSITIONS;
+            LEV_TRAFFIC = LEV_ROAD_CONNECTIONS | LEV_ROAD_SECTIONS | LEV_INTERSECTIONS | LEV_INTERSECTION_POSITIONS
+        }
 
-        public const uint BLOCK_TEXTURES = 0;
-        public const uint BLOCK_MODELS = 1;
-        public const uint BLOCK_WORLD = 2;
-        public const uint BLOCK_RANDOM_MODEL_PLACEMENT = 4;
-        public const uint BLOCK_TEXTURE_DEFINITIONS = 5;
-        public const uint BLOCK_ROAD_TABLE = 7;
-        public const uint BLOCK_ROAD_CONNECTIONS = 8;
-        public const uint BLOCK_INTERSECTIONS = 9;
-        public const uint BLOCK_HEIGHTMAP_TILES = 10;
-        public const uint BLOCK_HEIGHTMAP = 11;
-        public const uint BLOCK_MODELS_NAMES = 12;
-        public const uint BLOCK_EVENT_MODELS = 13;
-        public const uint BLOCK_VISIBILITY = 14;
-        public const uint BLOCK_SECTOR_TEXTURE_USAGE = 15;
-        public const uint BLOCK_ROAD_SECTIONS = 16;
-        public const uint BLOCK_INTERSECTION_POISITIONS = 17;
-        public const uint BLOCK_LAMPS = 19;
-        public const uint BLOCK_CHAIR_PLACEMENT = 20;
-        public const uint NUMBER_OF_BLOCKS = 21;
+        public enum Blocks
+        {
+            Textures = 0,
+            Models = 1,
+            World = 2,
+            RandomModelPlacement = 4,
+            TextureDefinitions = 5,
+            RoadTable = 7,
+            RoadConnections = 8,
+            Intersections = 9,
+            HeightmapTiles = 10,
+            Heightmap = 11,
+            ModelNames = 12,
+            EventModels = 13,
+            Visibility = 14,
+            SectorTextureUsage = 15,
+            RoadSections = 16,
+            IntersectionPositions = 17,
+            Lamps = 19,
+            ChairPlacement = 20,
+            NumberOfBlocks = 21
+        }
 
         protected int openBlocks { get; set; }
 
